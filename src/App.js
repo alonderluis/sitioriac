097db3a61b19ab4829591a48e84666e3416ddb14
tarea <alonder@gmail.com>
2023-07-21 10:46:03 -0500

    v04 actividad clase

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import C01componentes from './componentes/C01componentes';
 import C02contador from './componentes/C02contador';
-import P4variable from './componentes/P4variable';
+import C03dobleestado from './componentes/C03dobleestado';
+import C04variable from './componentes/C04variable';
+import C05operador from './componentes/C05operador';
+import C06matriz from './componentes/C06matriz';
 
 function App() {
   return (
@@ -15,11 +18,22 @@ function App() {
    
 
       <h1>componente 3</h1>
-      <P4variable></P4variable>
+      <C03dobleestado></C03dobleestado>
+
+      <h1>componente 4</h1>
+      <C04variable></C04variable>
+
+      <h1>componente 5</h1>
+      <C05operador></C05operador>
+
+      <h1>componente 6</h1>
+      <C06matriz></C06matriz>
+
+
+
     </div>
   );
 }
 
 export default App;
-
 
