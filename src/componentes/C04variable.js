e0fb31da72be6a9f5a5c2eeadb54dbe6955d19b0
Benja203 <benjamin.alvarado.1510@gmail>
2023-08-04 21:37:13 -0500

    examen

---
+++ src/componentes/C04variable.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const C04variable = (msj) => {
+    return (
+        <div>
+            <h1>Dando una lectura a una variable </h1>
+            <h2>Mensaje: {msj.xVariable}</h2>
+        </div>
+    );
+}
+
+export default C04variable;
